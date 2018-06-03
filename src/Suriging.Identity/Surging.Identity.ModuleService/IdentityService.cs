d4d352596710aebd7d27026671500a26d5a77ce3
iwaitu <iwaitucn@gmail.com>
2018-06-03 11:51:54 +0800

    ...

--- src/Suriging.Identity/Surging.Identity.ModuleService/IdentityService.cs
+++ src/Suriging.Identity/Surging.Identity.ModuleService/IdentityService.cs
@@ -18,32 +18,53 @@ namespace Surging.Identity.ModuleService
         private IdentityRepository _db = new IdentityRepository();
         private UserManager<SurgingUser> _usrManager;
         private UserStore<SurgingUser> _usrStore;
+        private IPasswordHasher<SurgingUser> passwordHasher = new PasswordHasher<SurgingUser>();
+        private UpperInvariantLookupNormalizer lookupNormalizer = new UpperInvariantLookupNormalizer();
 
         public IdentityService()
         {
             _usrStore = new UserStore<SurgingUser>(_db);
             _usrManager = new UserManager<SurgingUser>(_usrStore, null, null, null, null, null, null, null, null);
-
+            _usrManager.PasswordHasher = passwordHasher;
+            _usrManager.KeyNormalizer = lookupNormalizer;
         }
 
         public async Task<UserModel> Authentication(LoginModel requestData)
         {
-            var user = await _usrManager.FindByNameAsync(requestData.username);
-            if (user != null)
+            try
             {
-                var ret = await _usrManager.CheckPasswordAsync(user, requestData.password);
-                if (ret)
+                var user = await _usrManager.FindByNameAsync(requestData.username);
+                if (user != null)
                 {
-                    return user.ToModel();
+                    var ret = await _usrManager.CheckPasswordAsync(user, requestData.password);
+                    if (ret)
+                    {
+                        user.LastLoginDate = DateTime.Now;
+                        await _usrManager.UpdateAsync(user);
+                        return user.ToModel();
+                    }
+                    else
+                    {
+                        return null;
+                    }
                 }
-                else
-                {
-                    return null;
-                }
+                return null;
             }
-            return null;
+            catch (Exception ex)
+            {
 
+                return null;
+            }
+        }
 
+        private IUserPasswordStore<SurgingUser> GetPasswordStore()
+        {
+            var cast = _usrStore as IUserPasswordStore<SurgingUser>;
+            if (cast == null)
+            {
+                throw new NotSupportedException("fuck");
+            }
+            return cast;
         }
 
         public Task<bool> CheckPersonNoUnqine(string id)
@@ -58,14 +79,15 @@ namespace Surging.Identity.ModuleService
 
         public async Task<UserModel> CreateUser(string username, string password)
         {
-            var user = new SurgingUser();
-            user.SecurityStamp = Guid.NewGuid().ToString();
             var exituser = await _usrManager.FindByNameAsync(username);
             if (exituser != null)
                 return null;
+            var user = new SurgingUser();
+            user.SecurityStamp = Guid.NewGuid().ToString();
             user.UserName = username;
+            user.NormalizedUserName = _usrManager.KeyNormalizer.Normalize(username);
             user.PhoneNumber = username;
-            var passwordHasher = new PasswordHasher<SurgingUser>();
+            
             var pwhash = passwordHasher.HashPassword(user, password);
             user.PasswordHash = pwhash;
             var ret = await _usrStore.CreateAsync(user);
@@ -77,6 +99,12 @@ namespace Surging.Identity.ModuleService
             {
                 return null;
             }
+        }
+
+        public async Task<bool> CheckUserExist(string username)
+        {
+            var user = await _usrManager.FindByNameAsync(username);
+            return user != null;
         }
 
         public Task<UserModel> GetUserInfo(string id)
--- src/Suriging.Identity/Surging.Identity.xUnitTest/IdentityServiceTest.cs
+++ src/Suriging.Identity/Surging.Identity.xUnitTest/IdentityServiceTest.cs
@@ -1,3 +1,4 @@
+using Surging.Identity.IModuleService.ViewModels;
 using Surging.Identity.ModuleService;
 using System;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Surging.Identity.xUnitTest
     public class IdentityServiceTest
     {
         [Fact]
-        public async Task Test1()
+        public async Task CreateUser()
         {
             var service = new IdentityService();
             var user = await service.CreateUser("18107718055", "123456");
@@ -16,6 +17,24 @@ namespace Surging.Identity.xUnitTest
             ////var ret = await service.Authentication(new IModuleService.ViewModels.LoginModel { username = "18107711156", password = "scf1013" });
             //var ret = await service.CreateUser("rafael", "fafa888");
             //Assert.NotNull(ret);
+        }
+
+        [Fact]
+        public async Task UserLogin()
+        {
+            var loginModel = new LoginModel { username = "18107718055", password = "123456" };
+            var service = new IdentityService();
+            var user = await service.Authentication(loginModel);
+            Assert.NotNull(user);
+            Assert.True(user.UserName == "18107718055");
+        }
+
+        [Fact]
+        public async Task CheckUserExist()
+        {
+            var service = new IdentityService();
+            var ret = await service.CheckUserExist("18107718055");
+            Assert.True(ret);
         }
     }
 }
